692856728a0ea8f253f3ae3d8d7ad1c5a5645d5b
NAW-KHU <khusaywah2315@gmail.com>
2024-03-11 08:55:35 +0630

    modified files

---
+++ lib/Authentication/login_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class LoginPageScreen extends StatefulWidget {
+  const LoginPageScreen({super.key});
+
+  @override
+  State<LoginPageScreen> createState() => _LoginPageScreenState();
+}
+
+class _LoginPageScreenState extends State<LoginPageScreen> {
+  final username = TextEditingController();
+  final password = TextEditingController();
+  bool isVisible = false;
+
+  //login callback function
+  login(){
+    
+  }
+
+
+  final _formkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: SingleChildScrollView(
+              child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Form(
+                      key: _formkey,
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'lib/assets/login.png',
+                            width: 210,
+                          ),
+                          const SizedBox(height: 15),
+                          Container(
+                            margin: const EdgeInsets.all(8),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 6),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.deepPurple.withOpacity(0.2)),
+                            child: TextFormField(
+                              controller: username,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "username is required";
+                                }
+                                return null;
+                              },
+                              decoration: const InputDecoration(
+                                icon: Icon(Icons.person),
+                                border: InputBorder.none,
+                                hintText: 'Username',
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.all(8),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 6),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.deepPurple.withOpacity(0.2)),
+                            child: TextFormField(
+                              controller: password,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'password is required';
+                                }
+                                return null;
+                              },
+                              obscureText: !isVisible,
+                              decoration: InputDecoration(
+                                  icon: const Icon(Icons.lock),
+                                  border: InputBorder.none,
+                                  hintText: 'Password',
+                                  suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          //toggle button
+                                          isVisible = !isVisible;
+                                        });
+                                      },
+                                      icon: Icon(isVisible
+                                          ? Icons.visibility
+                                          : Icons.visibility_off))),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Container(
+                            height: 55,
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.deepPurple.withOpacity(0.5),
+                            ),
+                            child: TextButton(
+                                onPressed: () {
+                                  login();
+                                },
+                                child: const Text(
+                                  "LOGIN",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                          ),
+                          //sign up button
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text("Don't have an account?"),
+                              TextButton(
+                                onPressed: () {},
+                                child: const Text('SIGN UP'),
+                              )
+                            ],
+                          )
+                        ],
+                      ))))),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:user_login_sqflite/Authentication/login_screen.dart';
+
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,8 +23,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.blue),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       title: 'Flutter Login Page',
+      home: const LoginPageScreen(),
     );
   }
 }
